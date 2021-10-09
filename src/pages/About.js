470eb2f071717b8dcb6999635d40b87d13089352
Timothy Ayegbede <timtop99@gmail.com>
2021-10-10 00:36:29 +0100

    Began About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,24 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import "../Style/about.scss";
 import Navbar from "../Components/Navbar/Navbar";
 
 const HomePage = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      The About page <Link to="/">Home</Link>
-    </div>
+      <>
+        <div className="about">
+          <div className="about-head hd-text-w-smaller">
+            A bit more about me
+          </div>
+          <div className="container clearmargin">
+            I’m delited in bringing well designed interfaces to life to position
+            your business, brand or application at the forefront, I co-exist in
+            the world of design and web development
+          </div>
+        </div>
+      </>
+    </>
   );
 };
 
