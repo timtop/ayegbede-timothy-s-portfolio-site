996f3c4418cb5aa6f742c79786e8b2be9f1b6bf4
Timothy Ayegbede <timtop99@gmail.com>
2022-03-18 12:48:57 +0100

    Done the blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,16 +1,38 @@
 import React from "react";
-// import Footer from "../Components/Footer/Footer";
-// import Navbar from "../Components/Navbar/Navbar";
 import Layout from "../Layout/Layout";
+import { StaticImage } from "gatsby-plugin-image";
+import LineButton from "../Components/LineButton/LineButton";
+import "../Style/blog.scss";
 
 const Blog = () => {
   return (
     <Layout>
-      {/* <Navbar /> */}
       <div>
-        <p style={{ marginTop: " 90px" }}> This is the blog page</p>
+        <div className="blogg-bg">
+          <div className="container blog">
+            {/* <img src={success} alt="Message sent" className="msg-confirm-img" /> */}
+            <StaticImage
+              src="../images/wink.png"
+              alt="Design"
+              placeholder="blurred"
+              className="blog-img"
+              quality="100"
+            />
+            <p className="blog-text bd-text-w">
+              Hey! Thank you for visiting here, check back later for content on
+            </p>
+
+            <ul className="blog-tags bd-text-w">
+              <li className="design-badge">Design</li>
+              <li className="dev-badge">Dev</li>
+              <li className="faith-badge">Faith</li>
+              <li className="lifestyle-badge">Lifestyle</li>
+            </ul>
+
+            <LineButton content="Homepage" route="/" />
+          </div>
+        </div>
       </div>
-      {/* <Footer /> */}
     </Layout>
   );
 };
