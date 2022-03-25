77788bfb5adccece23090144572ee0243affef9b
Timothy Ayegbede <timtop99@gmail.com>
2022-03-25 20:57:31 +0100

    everything los right but its not actually

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,12 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Ayegbede Timothy's Portfolio Site",
+    title: "Timothy Ayegbede's Portfolio Site",
+    titleTemplate: "%s · The Real Hero",
+    description: "Welcome to my Timothy Ayegbede's Website.",
+    url: "https://www.timothyayegbede.com", // No trailing slash allowed!
+    image: "./images/Website Preview.jpg", // Path to the image placed in the 'static' folder, in the project's root directory.
+    twitterUsername: "@just_timothy_",
   },
   plugins: [
     {
@@ -11,10 +16,22 @@ module.exports = {
         trackingIds: [
           "G-J48CLD63B8", // Google Analytics / GA
         ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+
           // Defaults to https://www.googletagmanager.com
           origin: "https://nervous-cori-ab4648.netlify.app/",
         },
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,10 +2,33 @@ import React from "react";
 import Footer from "../Components/Footer/Footer";
 import Navbar from "../Components/Navbar/Navbar";
 import "./Layout.scss";
+import { Helmet } from "react-helmet";
 
 const Layout = ({ children }) => {
   return (
     <div className="layout">
+      <Helmet>
+        <link charSet="utf-8" />
+        <title>Timothy Ayegbede's Website</title>
+        <meta
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="../favicon/apple-touch-icon.png"
+        />
+        <meta
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="../favicon/favicon-32x32.png"
+        />
+        <meta
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../favicon/favicon-16x16.png"
+        />
+        <meta rel="manifest" href="../favicon/site.webmanifest" />
+      </Helmet>
       <Navbar />
       <div className="layout-content">{children}</div>
       <div className="layout-footer">
