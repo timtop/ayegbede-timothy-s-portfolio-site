dc186bd97bc27ef65fd203069008ccfa9fc5b230
Timothy Ayegbede <timtop99@gmail.com>
2022-03-30 18:25:50 +0100

    Update: Added the frontend section

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -3,29 +3,40 @@ import "./Cards.scss";
 import Img from "../Img/Img";
 import { Link } from "gatsby";
 
-const Cards = ({ image, title, role, route, pillDisplay, descDisplay }) => {
+const Cards = ({
+  image,
+  title,
+  role,
+  route,
+  pillDisplay,
+  descDisplay,
+  description,
+  link,
+}) => {
   return (
     <div className="card">
-      <Link to={route}>
-        <Img src={image} alt={title} width={600} height={400} />
-      </Link>
-      {/* <img src={image} alt="Moov Thumbnail" className="card-img" /> */}
-      <p className="card-title hd-text-w-smaller-small">{title}</p>
-      <div className="card-role">
-        {" "}
-        <span
-          style={{ display: pillDisplay ? "inline" : "none" }}
-          className="card-role bd-text-w design-badge"
-        >
-          {role}
-        </span>
-        <p
-          style={{ display: descDisplay ? "inline" : "none" }}
-          className="bd-text-w"
-        >
-          Description of the card where applicable
-        </p>
-      </div>
+      <a href={link} target="_blank" rel="noreferrer">
+        <Link to={route}>
+          <Img src={image} alt={title} width={600} height={400} />
+          {/* <img src={image} alt="Moov Thumbnail" className="card-img" /> */}
+          <p className="card-title hd-text-w-smaller-small">{title}</p>
+          <div className="card-role">
+            {" "}
+            <span
+              style={{ display: pillDisplay ? "inline" : "none" }}
+              className="card-role bd-text-w design-badge"
+            >
+              {role}
+            </span>
+            <p
+              style={{ display: descDisplay ? "inline" : "none" }}
+              className="bd-text-w"
+            >
+              {description}
+            </p>
+          </div>
+        </Link>
+      </a>
     </div>
   );
 };
--- src/Components/TabComponents/Tabs.js
+++ src/Components/TabComponents/Tabs.js
@@ -8,6 +8,9 @@ import moov from "../../images/thumbnails/Moov Thumbnail.jpg";
 import Delivered from "../../images/thumbnails/Its Delivered Thumbnail.png";
 // import Splufix from "../../images/thumbnails/Splufix Thumbnail.jpg";
 import dreampulze from "../../images/thumbnails/DreamPulze Thumbnail.jpg";
+import sermonPortal from "../../images/thumbnails/Sermon Portal Thumbnail.png";
+import g2m from "../../images/thumbnails/G2M Thumbnail.png";
+import timThumb from "../../images/thumbnails/Tim Thumbnail.png";
 
 import Cards from "../Cards/Cards";
 
@@ -17,6 +20,36 @@ const Tabs = () => {
   // Create the header ref
   const headerRef = useRef(0);
   // Get the position of my div
+
+  const frontEndCardItems = [
+    {
+      image: sermonPortal,
+      title: "Sermon Portal",
+      // role: "Product Designer",
+      description: "A central repository to download audio sermons",
+      // route: "./Design/Moov",
+      descDisplay: true,
+      link: "https://sharp-mccarthy-91907a.netlify.app/",
+    },
+    {
+      image: g2m,
+      title: "Grocery to Market",
+      // role: "UI Designer",
+      description: "An ecommerce platform for food items",
+      // route: "./Design/DreamPulze",
+      descDisplay: true,
+      link: "https://vigilant-bardeen-d428a0.netlify.app/",
+    },
+    {
+      image: timThumb,
+      title: "Ayegbede Timothy's Website",
+      // role: "UI Designer",
+      description: "My Personal Website",
+      // route: "./Design/Its-Delivered",
+      descDisplay: true,
+      link: "https://nervous-cori-ab4648.netlify.app/",
+    },
+  ];
 
   const cardItems = [
     {
@@ -116,8 +149,8 @@ const Tabs = () => {
         {/* Dev Tab Begins */}
         <TabContent id="tab2" activeTab={activeTab}>
           <div className="hd-text-w-smaller">Frontend Projects</div>
-          {/* <div className="card-holder">
-            {cardItems.map((item) => {
+          <div className="card-holder">
+            {frontEndCardItems.map((item) => {
               return (
                 <Cards
                   image={item.image}
@@ -125,11 +158,12 @@ const Tabs = () => {
                   role={item.role}
                   route={item.route}
                   descDisplay={item.descDisplay}
+                  description={item.description}
+                  link={item.link}
                 />
               );
             })}
-          </div> */}
-          Still under construction...
+          </div>
         </TabContent>
       </div>
     </div>
